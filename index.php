0e748c0a0aa737c34ce57090c078db34c51168ff
jamsheedkhalid <jamsheedkhalid35@gmail.com>
2019-12-10 15:09:39 +0400

    invoice

--- index.php
+++ index.php
@@ -26,8 +26,6 @@
     <link rel="stylesheet" type="text/css" href="js/print/print.min.css">
     <link rel="stylesheet" type="text/css" href="css/main.css">
 
-
-
 </head>
 <body>
 <h1 align="center" style="padding-top: 10px"><u>TAX INVOICE</u></h1>
@@ -89,14 +87,189 @@
                                 <br>
                                 <table align="right" >
                                     <tr><td align="right">
-                                            <h3 ><u><b>TAX INVOICE</b></u></h3>
+                                            <h3 style="color: green;"><u><b>TAX INVOICE</b></u></h3>
                                             <h16>TRN:10000002342345</h16>
                                         </td></tr>
                                 </table>
                             </div>
                         </div>
                         <hr>
-                        
+                        <div class="row" style="margin: 5px;">
+                            <div class="col-sm-9" style="border: solid black 1px; padding-top: 10px">
+                                Bill To <br>
+                                <table>
+                                    <tr>
+                                        <td><b>Name</b></td>
+                                        <td>: Mr/Mrs. John</td>
+                                    </tr>
+                                    <tr>
+                                        <td><b>Family Code</b></td>
+                                        <td>: 12345</td>
+                                    </tr>
+                                    <tr>
+                                        <td><b>Tel</b></td>
+                                        <td>: 050123456</td>
+                                    </tr>
+                                </table>
+
+                            </div>
+                            <div class="col-sm-3" style="border: solid black 1px;">
+                                <table>
+                                    <br><br>
+                                    <tr>
+                                        <td><b>Invoice No</b></td>
+                                        <td>: 123456</td>
+                                    </tr>
+                                    <tr>
+                                        <td><b>Invoice Date</b></td>
+                                        <td>: 30.12.2019</td>
+                                    </tr>
+                                </table>
+                            </div>
+                        </div>
+
+                        <div style="margin: 5px; padding-top: 25px">
+
+
+                            <div class="row">
+                                <div class="col-sm" align="left">
+                                    <b>Student: </b><label> 12345</label> - <label>Abdul Azeez</label>
+                                </div>
+                                <div class="col-sm" align="center">
+                                    <b>Grade: </b><label> GR 1 -2019</label>
+                                </div>
+                                <div class="col-sm" align="right">
+                                    <b>Academic Year: </b><label> AY 2019-2020</label>
+                                </div>
+                            </div>
+
+
+                            <table class="table table-bordered table-sm">
+                                <thead>
+                                <tr>
+                                    <th scope="col" colspan="3">Particular</th>
+                                    <th scope="col">Amount</th>
+                                    <th scope="col">VAT(%)</th>
+                                    <th scope="col">VAT Amount</th>
+                                    <th scope="col">Total</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <tr>
+                                    <td colspan="3">Uniform</td>
+                                    <td align="right">90</td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td colspan="3">Books</td>
+                                    <td align="right">1200</td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td colspan="3">Tuition Fees</td>
+                                    <td align="right"> 40000</td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td colspan="3">Transportation</td>
+                                    <td align="right"> 1000</td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                </tr>
+                                </tbody>
+                            </table>
+
+
+                            <div class="row">
+                                <div class="col-sm-4" align='left'
+                                     style="border: solid black 1px; margin: 10px; padding: 5px; font-size: 14px">
+                                    <u>Tuition Fee Description:</u>
+                                    <table class="">
+                                        <tr>
+                                            <td>1st Installment</td>
+                                            <td>:</td>
+                                            <td>5000.00</td>
+                                        </tr>
+                                        <tr>
+                                            <td>2nd Installment</td>
+                                            <td>:</td>
+                                            <td>8000.00</td>
+                                        </tr>
+                                        <tr>
+                                            <td>3nd Installment</td>
+                                            <td>:</td>
+                                            <td>9000.00</td>
+                                        </tr>
+                                    </table>
+                                </div>
+                                <div class="col-sm"></div>
+                                <div class="col-sm" align="right">
+                                    <table class="table table-bordered table-sm">
+                                        <tr>
+                                            <td>Total(AED)</td>
+                                            <td align="right">105000.00</td>
+                                        </tr>
+                                        <tr>
+                                            <td>VAT Total(AED)</td>
+                                            <td align="right">500.00</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Net Total(AED)</td>
+                                            <td align="right">105500.00</td>
+                                        </tr>
+                                    </table>
+                                </div>
+                            </div>
+
+                            <div class="row">
+                                <div class="col-sm">
+                                    Bank Details: <br>
+                                    <table style="border-collapse: collapse; border: 1px solid black; font-size: 14px">
+                                        <tr>
+                                            <td>Bank Name</td>
+                                            <td>Bank of Sharjah</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Branch</td>
+                                            <td>Al Ain</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Account Name</td>
+                                            <td>Al Sanawbar School</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Account No</td>
+                                            <td>123456987</td>
+                                        </tr>
+                                        <tr>
+                                            <td>IBAN</td>
+                                            <td>AES1458789658</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Currency</td>
+                                            <td>AED</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Swift Code</td>
+                                            <td>SHA21231</td>
+                                        </tr>
+                                    </table>
+                                </div>
+
+                                <div class="col-sm">
+                                    One of three columns
+                                </div>
+                            </div>
+
+
+                        </div>
 
 
                     </div>
@@ -104,8 +277,10 @@
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary"  id='printbtn'
-                        onclick="printJS({printable: 'invoicePrint', type: 'html', header: null, documentTitle: null, ignoreElements: ['', ''], honorColor: true, css: 'css/print.css'})">PRINT</button>
+                <button type="button" class="btn btn-primary" id='printbtn'
+                        onclick="printJS({printable: 'invoicePrint', type: 'html', header: null, documentTitle: null })">
+                    PRINT
+                </button>
             </div>
         </div>
     </div>
