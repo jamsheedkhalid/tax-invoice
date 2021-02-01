e104d7f085f31a2ee0b189fefae8df2f76ca087e
Hesham <heshamoov90@gmail.com>
2021-02-01 11:22:33 +0400

    Add Rahman and Reem

--- include/loginFunction.php
+++ include/loginFunction.php
@@ -28,7 +28,7 @@ function login()
         } else {
             $sql = "SELECT
             *
-            FROM users WHERE id='$user' AND ( username = 'James' OR username = 'admin' OR username = 'Hesham') ";
+            FROM users WHERE id='$user' AND ( username = 'James' OR username = 'admin' OR username = 'Hesham' OR 'reem' OR 'Rahman') ";
 
 //        echo $sql;
             $result = $conn->query($sql);
